11e15d52d4f5c9883d7a98cfffeea2fd4dbd9dd6
JonahChoon <jonahchooncm@gmail.com>
2026-04-21 09:53:24 +0800

    Fix: updated base path for root domain

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/jc-portfolio/', // This must match your repository name
+  plugins: [react()],
+  base: '/', // <--- Make sure this is just a forward slash
 })
