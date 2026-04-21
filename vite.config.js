bdc6de7fc6d5920231e5cd92341b14af79a7e672
JonahChoon <jonahchooncm@gmail.com>
2026-04-21 09:57:16 +0800

    Fix asset paths for root domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // <--- Make sure this is just a forward slash
+  base: './', // Change this from '/jc-portfolio/' to './' or '/'
 })
